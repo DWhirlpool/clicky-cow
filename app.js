e0f5724cd65e43ed21e06c9f183ce2184f50e5a2
Daniel Lu <ludaniel20110@outlook.com>
2022-11-04 12:25:32 +0000

    fix test

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var cowclicks = parseInt(localStorage.getItem("cowclicks"));
 var cowclick = function() {
     parseInt(cowclicks += 1)
     localStorage.setItem("cowclicks", cowclicks)
-}
-var referesh = function() {
-    var currentClicks = localStorage.getItem("cowclicks")
-    document.getElementById("clicks").textContent = currentClicks;
     if (localStorage.getItem("cowclicks") == NaN) {
         localStorage.setItem("cowclicks", 0)
         console.log(test)
     }
+}
+var referesh = function() {
+    var currentClicks = localStorage.getItem("cowclicks")
+    document.getElementById("clicks").textContent = currentClicks;
 }
 setInterval(referesh,0.1)
