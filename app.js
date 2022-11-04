d47aa539c52bb9aeac9c0cecb0b5613f0dd2f66b
Daniel Lu <ludaniel20110@outlook.com>
2022-11-04 12:45:43 +0000

    fix?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var cowname = "Clicky";
 var cowclicks = 0
 var cowclick = function() {
-    parseInt(cowclicks += 1)
+    parseInt(cowclicks += 0.00364963504)
     localStorage.setItem("cowclicks", parseInt(localStorage.getItem("cowclicks")) + cowclicks)
 }
 var referesh = function() {
