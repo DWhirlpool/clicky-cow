af479e426b3c28bd073d2fd916359a53efdbd577
Daniel Lu <ludaniel20110@outlook.com>
2022-11-04 12:23:08 +0000

    fix

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var cowname = "Clicky";
 var cowclicks = parseInt(localStorage.getItem("cowclicks"));
-if (localStorage.getItem("cowclicks") == NaN) {
-    localStorage.setItem("cowclicks", 0)
-}
 var cowclick = function() {
     parseInt(cowclicks += 1)
     localStorage.setItem("cowclicks", cowclicks)
